66d48e0cebb7da31d631a7469bda14acaec698c8
ekuzneczova1988 <e.kuzneczova1988@gmail.com>
2015-12-20 20:38:39 +0600

    Описана модель данных

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ Base = declarative_base()
 
     
 class Manufacture(Base):
-    __tablename__ = 'manufacture'
+    __tablename__ = 'manufactures'
 
     sqlite_autoincrement = True
     id = Column(Integer, primary_key=True)
@@ -24,3 +24,46 @@ class Manufacture(Base):
 
     last_update = Column(DateTime, default=datetime.datetime.now)
 
+class Model(Base):
+    __tablename__ = 'models'
+
+    sqlite_autoincrement = True
+    id = Column(Integer, primary_key=True)
+
+    name = Column(String(160))
+    link = Column(String(160))
+    bodyid = Column(Integer)
+    startman = Column(String(100))
+    stopman = Column(String(100))
+    modelid = Column(Integer)
+    manid = Column(Integer)
+
+    last_update = Column(DateTime, default=datetime.datetime.now)
+    
+class BodyDictionary(Base):
+    __tablename__ = 'bodydictionaries'
+
+    sqlite_autoincrement = True
+    id = Column(Integer, primary_key=True)
+
+    name = Column(String(160))
+    modelid = Column(String(160))
+
+    last_update = Column(DateTime, default=datetime.datetime.now) 
+    
+class Modification(Base):
+    __tablename__ = 'modifications'
+
+    sqlite_autoincrement = True
+    id = Column(Integer, primary_key=True)
+
+    name = Column(String(160))
+    productionyear = Column(String(160))
+    modelid = Column(Integer)
+    kw = Column(Integer)
+    hp = Column(Integer)
+    volume = Column(Integer)
+    type = Column(String(160))
+    link = Column(String(160))
+
+    last_update = Column(DateTime, default=datetime.datetime.now) 
